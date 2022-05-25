b75e5dcfcc42104bc7cf5dbc6f01160c51646184
aed20 <43352424+aed20@users.noreply.github.com>
2022-05-25 10:47:25 -0500

    Added files

---
+++ DoAsI'mDoing/DoAsI'mDoing/Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	//I decided to make each question and each answer a seperate line.
+	cout << "\t\t\t\tBasic BYU Trivia\n";
+	cout << "\t\tQuestions";
+	cout << "\t\t\t\tAnswers\n\n";
+	cout << "What was the original name of BYU?";
+	cout << "\t\tBrigham Young Academy\n";
+	cout << "When was BYA established?";
+	cout << "\t\t\t1875\n";
+	cout << "Who was the first \"permanent\" principal of BYA?";
+	cout << "\tKarl Maeser\n";
+	cout << "When did BYA become BYU?";
+	cout << "\t\t\t1903\n";
+	cout << "To what sports conference do we belong?";
+	cout << "\t\tIndependent (Football)\n";
+	cout << "When did BYU win the national football title?";
+	cout << "\t1984\n";
+	cout << "Who won the Heisman Trophy in 1990?";
+	cout << "\t\tTy Detmer\n\n";
+	system("pause");
+	return 0;
+}
